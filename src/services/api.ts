5e11c408eb564ba502aa879d5fac76b62f05b2d1
Israel Carneiro <isragcarneiro@gmail.com>
2022-05-30 01:25:56 -0300

    deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: '/api'
+  baseURL: '/api',
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}`
+  }
 })
