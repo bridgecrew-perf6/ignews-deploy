0eba0eb55a1a75c579034808065be3baaf800a5d
Israel Carneiro <isragcarneiro@gmail.com>
2022-05-30 01:54:02 -0300

    changing api file

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: '/api',
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}`
+    Authorization: `Bearer ${process.env.STRIPE_API_KEY}`
   }
 })
