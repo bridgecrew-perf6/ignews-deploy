b49d63afb2c55770a499a1f3221edcc5ab9f6d96
Israel Carneiro <isragcarneiro@gmail.com>
2022-05-30 01:48:08 -0300

    deploy 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { SessionProvider as NextAuthProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
 import Link from 'next/link'
 
-import { linkResolver, repositoryName } from '../../prismicio'
+import { repositoryName } from '../../prismicio'
 import { Header } from '../components/Header'
 
 import '../styles/global.scss'
@@ -15,19 +15,16 @@ export default function App({
 }: AppProps) {
   return (
     <PrismicProvider
-      linkResolver={linkResolver}
       internalLinkComponent={({ href, children, ...props }) => (
         <Link href={href}>
           <a {...props}>{children}</a>
         </Link>
       )}
     >
-      <PrismicPreview repositoryName={repositoryName}>
-        <NextAuthProvider session={session}>
-          <Header />
-          <Component {...pageProps} />
-        </NextAuthProvider>
-      </PrismicPreview>
+      <NextAuthProvider session={session}>
+        <Header />
+        <Component {...pageProps} />
+      </NextAuthProvider>
     </PrismicProvider>
   )
 }
